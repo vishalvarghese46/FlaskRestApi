42d44b58d2da9729f344e5635071a4cfe0021654
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-05-05 21:44:40 +0100

    Finishing the Bird class

--- main.py
+++ main.py
@@ -6,14 +6,143 @@ from collections import deque
 import pygame
 from pygame.locals import *
 
-#base variable
 FPS = 60                #Frame size of the program
 ANIMATION_SPEED = 0.18  #The animation speed of the program
 
 WIN_WIDTH = 284 * 2
 WIN_HEIGHT = 512
 
+
+class Bird(pygame.sprite.Sprite):
+    """Represents the bird controlled by the player.
+
+    The bird is the 'hero' of this game.  The player can make it climb
+    (ascend quickly), otherwise it sinks (descends more slowly).  It must
+    pass through the space in between pipes (for every pipe passed, one
+    point is scored); if it crashes into a pipe, the game ends.
+
+    Attributes:
+    x: The bird's X coordinate.
+    y: The bird's Y coordinate.
+    msec_to_climb: The number of milliseconds left to climb, where a
+        complete climb lasts Bird.CLIMB_DURATION milliseconds.
+
+    Constants:
+    WIDTH: The width, in pixels, of the bird's image.
+    HEIGHT: The height, in pixels, of the bird's image.
+    SINK_SPEED: With which speed, in pixels per millisecond, the bird
+        descends in one second while not climbing.
+    CLIMB_SPEED: With which speed, in pixels per millisecond, the bird
+        ascends in one second while climbing, on average.  See also the
+        Bird.update docstring.
+    CLIMB_DURATION: The number of milliseconds it takes the bird to
+        execute a complete climb.
+    """
+
+    WIDTH = HEIGHT = 50
+    SINK_SPEED = 0.18
+    CLIMB_SPEED = 0.3
+    CLIMB_DURATION = 333.3
+
+    def __init__(self, x, y, msec_to_climb, images):
+        """Initialise a new Bird instance.
+
+        Arguments:
+        x: The bird's initial X coordinate.
+        y: The bird's initial Y coordinate.
+        msec_to_climb: The number of milliseconds left to climb, where a
+            complete climb lasts Bird.CLIMB_DURATION milliseconds.  Use
+            this if you want the bird to make a (small?) climb at the
+            very beginning of the game.
+        images: A tuple containing the images used by this bird.  It
+            must contain the following images, in the following order:
+                0. image of the bird with its wing pointing upward
+                1. image of the bird with its wing pointing downward
+        """
+        super(Bird, self).__init__()
+        self.x, self.y = x, y
+        self.msec_to_climb = msec_to_climb
+        self._img_wingup, self._img_wingdown = images
+        self._mask_wingup = pygame.mask.from_surface(self._img_wingup)
+        self._mask_wingdown = pygame.mask.from_surface(self._img_wingdown)
+
+    def update(self, delta_frames=1):
+        """Update the bird's position.
+
+        This function uses the cosine function to achieve a smooth climb:
+        In the first and last few frames, the bird climbs very little, in the
+        middle of the climb, it climbs a lot.
+        One complete climb lasts CLIMB_DURATION milliseconds, during which
+        the bird ascends with an average speed of CLIMB_SPEED px/ms.
+        This Bird's msec_to_climb attribute will automatically be
+        decreased accordingly if it was > 0 when this method was called.
+
+        Arguments:
+        delta_frames: The number of frames elapsed since this method was
+            last called.
+        """
+        if self.msec_to_climb > 0:
+            frac_climb_done = 1 - self.msec_to_climb/Bird.CLIMB_DURATION
+            self.y -= (Bird.CLIMB_SPEED * frames_to_msec(delta_frames) *
+                       (1 - math.cos(frac_climb_done * math.pi)))
+            self.msec_to_climb -= frames_to_msec(delta_frames)
+        else:
+            self.y += Bird.SINK_SPEED * frames_to_msec(delta_frames)
+
+    @property
+    def image(self):
+        """Get a Surface containing this bird's image.
+
+        This will decide whether to return an image where the bird's
+        visible wing is pointing upward or where it is pointing downward
+        based on pygame.time.get_ticks().  This will animate the flapping
+        bird, even though pygame doesn't support animated GIFs.
+        """
+        if pygame.time.get_ticks() % 500 >= 250:
+            return self._img_wingup
+        else:
+            return self._img_wingdown
+
+    @property
+    def mask(self):
+        """Get a bitmask for use in collision detection.
+
+        The bitmask excludes all pixels in self.image with a
+        transparency greater than 127."""
+        if pygame.time.get_ticks() % 500 >= 250:
+            return self._mask_wingup
+        else:
+            return self._mask_wingdown
+
+    @property
+    def rect(self):
+        """Get the bird's position, width, and height, as a pygame.Rect."""
+        return Rect(self.x, self.y, Bird.WIDTH, Bird.HEIGHT)
+
 def load_images():
+    '''load images required by images and return a dict of them'''
+
+
+
+
+    def load_image(img_file_name):
+        file_name = os.path.join('.','image',img_file_name)
+        img = pygame.image.load(file_name)
+        img.convert()
+        return img
+
+    return {
+        'background': load_image('background.png'),
+        'bird_wing_up': load_image('bird_wing_up.png'),
+        'bird_wing_down': load_image('bird_wing_down.png'),
+        'pipe_body': load_image('pipe_body.png'),
+        'pipe_end': load_image('pipe_end.png')
+    }
+
+
+
+
+
 
 
 def main():
