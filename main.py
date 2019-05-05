bc41bf14566ee899de2ff67abba716ee8323bdcf
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-05-05 20:37:22 +0100

    Adding all image files, initializing pygame objects

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import math
+import os
+from random import randint
+from collections import deque
+
 import pygame
+from pygame.locals import *
+
+#base variable
+FPS = 60                #Frame size of the program
+ANIMATION_SPEED = 0.18  #The animation speed of the program
+
+WIN_WIDTH = 284 * 2
+WIN_HEIGHT = 512
+
+def load_images():
+
+
+def main():
+    pygame.init()
+    display_sufrace = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    pygame.display.set_caption('Flappy Bird')
+
+    clock = pygame.time.Clock()
+    score_font = pygame.font.SysFont(None,32,bold=True)
+    images = load_images()
+
+
+
+
+
+
