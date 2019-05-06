4f91e58bb817fa9133cb13a40b0b42b89c2b77b4
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-05-06 21:24:28 +0100

    Creating the pipe class

--- main.py
+++ main.py
@@ -141,7 +141,85 @@ def load_images():
 
 
 
+class PipePair(pygame.sprite.Sprite):
+    """Represents an obstacle.
 
+    A PipePair has a top and a bottom pipe, and only between them can
+    the bird pass -- if it collides with either part, the game is over.
+
+    Attributes:
+    x: The PipePair's X position.  This is a float, to make movement
+        smoother.  Note that there is no y attribute, as it will only
+        ever be 0.
+    image: A pygame.Surface which can be blitted to the display surface
+        to display the PipePair.
+    mask: A bitmask which excludes all pixels in self.image with a
+        transparency greater than 127.  This can be used for collision
+        detection.
+    top_pieces: The number of pieces, including the end piece, in the
+        top pipe.
+    bottom_pieces: The number of pieces, including the end piece, in
+        the bottom pipe.
+
+    Constants:
+    WIDTH: The width, in pixels, of a pipe piece.  Because a pipe is
+        only one piece wide, this is also the width of a PipePair's
+        image.
+    PIECE_HEIGHT: The height, in pixels, of a pipe piece.
+    ADD_INTERVAL: The interval, in milliseconds, in between adding new
+        pipes.
+    """
+
+    WIDTH = 80
+    PIECE_HEIGHT = 32
+    ADD_INTERVAL = 3000
+
+    def __init__(self, pipe_end_img, pipe_body_img):
+        """Initialises a new random PipePair.
+
+        The new PipePair will automatically be assigned an x attribute of
+        float(WIN_WIDTH - 1).
+
+        Arguments:
+        pipe_end_img: The image to use to represent a pipe's end piece.
+        pipe_body_img: The image to use to represent one horizontal slice
+            of a pipe's body.
+        """
+        self.x = float(WIN_WIDTH - 1)
+        self.score_counted = False
+
+        self.image = pygame.Surface((PipePair.WIDTH, WIN_HEIGHT), SRCALPHA)
+        self.image.convert()   # speeds up blitting
+        self.image.fill((0, 0, 0, 0))
+        total_pipe_body_pieces = int(
+            (WIN_HEIGHT -                  # fill window from top to bottom
+             3 * Bird.HEIGHT -             # make room for bird to fit through
+             3 * PipePair.PIECE_HEIGHT) /  # 2 end pieces + 1 body piece
+            PipePair.PIECE_HEIGHT          # to get number of pipe pieces
+        )
+        self.bottom_pieces = randint(1, total_pipe_body_pieces)
+        self.top_pieces = total_pipe_body_pieces - self.bottom_pieces
+
+        # bottom pipe
+        for i in range(1, self.bottom_pieces + 1):
+            piece_pos = (0, WIN_HEIGHT - i*PipePair.PIECE_HEIGHT)
+            self.image.blit(pipe_body_img, piece_pos)
+        bottom_pipe_end_y = WIN_HEIGHT - self.bottom_height_px
+        bottom_end_piece_pos = (0, bottom_pipe_end_y - PipePair.PIECE_HEIGHT)
+        self.image.blit(pipe_end_img, bottom_end_piece_pos)
+
+        # top pipe
+        for i in range(self.top_pieces):
+            self.image.blit(pipe_body_img, (0, i * PipePair.PIECE_HEIGHT))
+        top_pipe_end_y = self.top_height_px
+        self.image.blit(pipe_end_img, (0, top_pipe_end_y))
+
+        # compensate for added end pieces
+        self.top_pieces += 1
+        self.bottom_pieces += 1
+
+        # for collision detection
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 
