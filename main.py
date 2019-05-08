199ec889d25fae490ad7d8bfaedbe2c9562148bc
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-05-08 22:30:09 +0100

    Loading all images for the game

--- main.py
+++ main.py
@@ -222,6 +222,46 @@ class PipePair(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
 
+def load_images():
+    """Load all images required by the game and return a dict of them.
+
+    The returned dict has the following keys:
+    background: The game's background image.
+    bird-wingup: An image of the bird with its wing pointing upward.
+        Use this and bird-wingdown to create a flapping bird.
+    bird-wingdown: An image of the bird with its wing pointing downward.
+        Use this and bird-wingup to create a flapping bird.
+    pipe-end: An image of a pipe's end piece (the slightly wider bit).
+        Use this and pipe-body to make pipes.
+    pipe-body: An image of a slice of a pipe's body.  Use this and
+        pipe-body to make pipes.
+    """
+
+    def load_image(img_file_name):
+        """Return the loaded pygame image with the specified file name.
+
+        This function looks for images in the game's images folder
+        (./images/).  All images are converted before being returned to
+        speed up blitting.
+
+        Arguments:
+        img_file_name: The file name (including its extension, e.g.
+            '.png') of the required image, without a file path.
+        """
+        file_name = os.path.join('.', 'images', img_file_name)
+        img = pygame.image.load(file_name)
+        img.convert()
+        return img
+
+    return {'background': load_image('background.png'),
+            'pipe-end': load_image('pipe_end.png'),
+            'pipe-body': load_image('pipe_body.png'),
+            # images for animating the flapping bird -- animated GIFs are
+            # not supported in pygame
+            'bird-wingup': load_image('bird_wing_up.png'),
+            'bird-wingdown': load_image('bird_wing_down.png')}
+
+
 
 def main():
     pygame.init()
