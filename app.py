0678271f0a82fd7f87588261373a4589d79f918b
Vishal <vishalvarghese46@gmail.com>
2021-03-22 20:38:41 +0000

    addin run.py file

--- app.py
+++ app.py
@@ -11,12 +11,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.secret_key = 'vishalmon'
-api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    
+api = Api(app)   
 jwt = JWT(app,authenticate,identity)
 
 @app.route("/")
@@ -28,8 +23,3 @@ api.add_resource(ItemList,"/items")
 api.add_resource(Store,"/store/<string:name>")
 api.add_resource(StoreList,"/stores")
 api.add_resource(UserRegister,'/register')
-
-if __name__=="__main__":
-    from db import db
-    db.init_app(app)
-    app.run(debug=True,port=5000)
