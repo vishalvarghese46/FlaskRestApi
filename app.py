d27300d96d779b9dc4083d140e25bdd1d525c49d
Vishal <vishalvarghese46@gmail.com>
2021-03-22 21:26:27 +0000

    adding postgres extension

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,jsonify
 from flask_restful import Api
 from flask_jwt import JWT, jwt_required
@@ -9,7 +10,7 @@ from resources.store import Store,StoreList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///data.db")
 app.secret_key = 'vishalmon'
 api = Api(app)   
 jwt = JWT(app,authenticate,identity)
